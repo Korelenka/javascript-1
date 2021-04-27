1e64ee248c1aa219cd0892f71a653597667c541e
aud203 <ifmo@gmail.com>
2021-04-27 21:50:15 +0300

    events ++

--- lesson10-11/js/form-events.js
+++ lesson10-11/js/form-events.js
@@ -1,1 +1,48 @@
 'use strict';
+// document.forms - все формы в документе
+// lesson - значение атрибута name нужной формы
+let form = document.forms.lesson;
+console.log(form);
+
+// доступ к элементам формы по значению атрибута name
+// form.elements - все элементы формы
+// login - значение атрибута name нужного элемента формы
+let loginInput = form.elements.login;
+console.log(loginInput);
+console.log(loginInput.value);
+loginInput.value = "qwerty";
+
+let passwordInput = form.elements.pwd;
+console.log(passwordInput);
+
+//
+let colorRadios = form.elements.color;
+for (let radio of colorRadios) {
+    radio.addEventListener("focus", changeColor);
+}
+
+function changeColor(){
+    form.elements.checkbox_fieldset.style.background = this.value;
+}
+
+// name="lang[]"
+let langCheckBoxes = form.elements["lang[]"];
+for (let checkBox of langCheckBoxes) {
+    if (checkBox.checked) console.log(checkBox.value);
+}
+
+let select = form.elements.countries;
+for (let option of select) {
+    if (option.selected) console.log(option.value);
+}
+
+// событие - отправка формы
+form.addEventListener("submit", submitForm);
+
+function submitForm(event) {
+    // отмена события по умолчанию
+    event.preventDefault();
+
+    console.log(loginInput.value);
+    console.log(passwordInput.value);
+}
--- lesson10-11/js/lesson10.js
+++ lesson10-11/js/lesson10.js
@@ -13,5 +13,48 @@ function openDescription() {
     this.nextElementSibling.classList.toggle("open");
 }
 
+// data-*
+// data-prise="dog"
+// получение значения data атрибута:
+// елемент.dataset.prise // dog
+
+let getPresent = () => {
+    return {
+        dog: "Собака",
+        book: "Книга",
+        car: "Машина"
+    }
+}
+
+let presentContainer = document.getElementById("present_container");
+
+presentContainer.addEventListener("click", showPresent);
+// showPresent(объект-события)
+
+function showPresent(event){
+    console.log(event);
+    // элемент, на котором произошло событие event.target
+    let clickElem = event.target;
+    console.log(clickElem);
+    let presentValue = clickElem.dataset.prise;
+    console.log(presentValue);
+    if (presentValue !== undefined){
+        let prises = getPresent();
+        // console.log(prises[presentValue]);
+        clickElem.innerText = prises[presentValue];
+        clickElem.classList.add("present");
+        // удаление обработчика события
+        // presentContainer.addEventListener("click", showPresent);
+        this.removeEventListener("click", showPresent);
+        // presentContainer.removeEventListener("click", showPresent);
+    }
+}
+
+
+
+
+
+
+
 
 
